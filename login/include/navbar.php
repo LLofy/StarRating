96fc54b2ee63084ce78ca703ea485f5b7822b45b
LLofy <45644153+LLofy@users.noreply.github.com>
2021-06-03 23:07:51 -0300

    Delete login/include directory

--- login/include/navbar.php
+++
@@ -1,8 +0,0 @@
-<nav class="navtop">
-			<div>
-				<h1><a  href=home.php>Website Title</a></h1>
-				<a href="books.php"><i class="far fa-star"></i>Livros</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
