dda959b7d0bfceb71af44edfee8e0d4e34ca5e19
LLofy <45644153+LLofy@users.noreply.github.com>
2021-06-05 22:46:00 -0300

    Update book.php

--- login/book.php
+++ login/book.php
@@ -46,12 +46,14 @@
 			$mysqli = mysqli_connect('localhost', 'root', '', 'phpreviews');
 
 			// Get the total number of records from our table "books".
-			$total_pages = $mysqli->query('SELECT COUNT(*) FROM  reviews')->fetch_row()[0];
 
 			$pages = $mysqli->query('SELECT COUNT(*) FROM  reviews')->fetch_row()[0];
 
 			// Check if the page number is specified and check if it's a number, if not return the default page number which is 1.
 			$page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
+		
+			$total_pages = $mysqli->query('SELECT COUNT(*) FROM  reviews')->fetch_row()[0];
+			//where page_id = page
 			
 			// Number of results to show on each page.
 			$num_results_on_page = $pages;        
